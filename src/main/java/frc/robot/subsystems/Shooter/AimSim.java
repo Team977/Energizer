c35a94df5fda7eaeeada334f1a73a6f4e1ffd36e
billnye01 <105652816+billnye01@users.noreply.github.com>
2024-06-22 19:10:47 -0400

    save

---
+++ src/main/java/frc/robot/subsystems/Shooter/AimSim.java
@@ -0,0 +1,21 @@
+package frc.robot.subsystems.Shooter;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.CANSparkLowLevel.MotorType;
+
+import frc.robot.subsystems.MotorConst;
+
+public class AimSim {
+    
+    static final CANSparkMax AimMotor = new CANSparkMax(MotorConst.AimMotorCANId, MotorType.kBrushless);
+    static final RelativeEncoder ENCODER = AimMotor.getEncoder();
+    /** Creates a new Aim. */
+    public AimSim() {}
+  
+    public void setSpeed(double speed){
+      AimMotor.set(speed);
+    }
+
+
+}
